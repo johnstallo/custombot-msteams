6707ee3662d8d37d4077b7e5cbab3aff06799dc9
John Stallo <john.stallo@live.com>
2017-11-14 13:01:06 -0800

    clean up server.js

--- botservice/server.js
+++ botservice/server.js
@@ -22,28 +22,3 @@ app.post('/api/hello', function (req, res) {
         });
     });
 });
-
-    
-    // // Call 'metrics' service (ASP.NET WebAPI)
-    // request.get('http://metrics/api/server', function(err, response, body) {
-
-    //     // Include response from /metrics service
-    //     res.send({
-    //         "type": "message",
-    //         "text": "Hello " + req.body.from.name.split(' ')[0] + ", here's current server status: " + body
-    //     });
-    // });
-
-
-
-
-    // CODE SNIPPET
-    // // Call 'metrics' service (ASP.NET WebAPI)
-    // request.get('http://metrics/api/server', function(err, response, body) {
-
-    //     // Include response from /metrics service
-    //     res.send({
-    //         "type": "message",
-    //         "text": "Hello " + req.body.from.name.split(' ')[0] + ", here's current server status: " + body
-    //     });
-    // });
